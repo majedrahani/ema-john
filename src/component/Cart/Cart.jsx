d141af79aa33af12d0b7947134acf8af7154ce72
Your Name <you@example.com>
2023-04-02 19:28:29 +0600

    quantitty updated

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -7,9 +7,12 @@ const Cart = (props) => {
 
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total+ product.price;
+        product.quantity = product.quantity || 1;
+        total = total+ product.price *product.quantity;
         totalShipping = totalShipping+ product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = total * 20 / 100;
     const grandTotal = total+ totalShipping+ tax;
@@ -17,7 +20,7 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h4>Order Summery</h4>
-            <p>Selected Items : {cart.length}</p>
+            <p>Selected Items : {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge : ${totalShipping}</p>
             <p>Tax : ${tax}</p>
